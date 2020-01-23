858bc6385e82d7d5c4e4a3d0d81c1e7b56557fed
Romena Murgor <romenamurgor@gmail.com>
2020-01-23 18:20:50 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,14 +1,1 @@
- # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
-Failures:
- 
-  1) ./variable.rb defined a local variable called greeting and set it equal to 'Hello World'
-     Failure/Error: greeting = get_variable_from_file('./variable.rb', "greeting")
-     NameError:
-       local variable `greeting' not defined in ./variable.rb.
-     # ./spec/spec_helper.rb:14:in `rescue in get_variable_from_file'
-     # ./spec/spec_helper.rb:11:in `get_variable_from_file'
-     # ./spec/variable_spec.rb:5:in `block (2 levels) in <top (required)>'
- 
-Finished in 0.00075 seconds (files took 0.0839 seconds to load)
-1 example, 1 failure
+ variabl
